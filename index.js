65802f03b753093542c9f712ab7bb339c3d306bf
Carver Harrison <ch@dorper.me>
2019-02-14 20:11:34 -0800

    the quick brown fox jumps over the lazy dog 1234567890 times

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
+var config = require("./config.json");
+
+config.string = {
+	width: `${config.screen.width / 1.5}px`,
+	height: `${config.screen.height / 1.5}px`
+};
 
 var app = express();
 
@@ -15,22 +22,59 @@ app.get("/api/pathfiles", function(req, res) {
 	res.send({
 		files: pathFiles,
 		status: true
-	})
+	});
 });
 
-app.post("/api/pathfiles", function(req, res) {
+app.get("/api/pathfile", function(req, res) {
 	if (req.body.name) {
-		if (pathFiles.includes(req.body.name)) {
-			res.send({status: false, error: "file already in array"});
-			return;
-		}
-		pathFiles.push(req.body.name);
-		res.send({status: true});
+		request.get(`http://tegra-ubuntu.local:5800/sv/api/v1.0/property?name=${req.body.name}`, function(error, response, body) {
+			res.send({status: true, points: body});
+		});
 	} else{
 		res.send({status: false, error: "missing param name"});
 	}
 });
 
-app.listen("8080", function() {
+app.post("/api/pathfiles", function(req, res) {
+	if (req.body.name) {
+		pathFiles.push(req.body.name);
+		console.log(req.body.points);
+		request.post(`http://tegra-ubuntu.local:5800/sv/api/v1.0/set-property?name=${req.body.name}`, {json: {points: req.body.points}}, function(error, response, body) {
+			res.send({status: true});
+		});
+	} else{
+		res.send({status: false, error: "missing param name"});
+	}
+});
+
+app.get("/api/config", function(req, res) {
+	res.send(config);
+});
+
+app.post("/api/calculatesplines", function(req, res) {
+	var csvPath = "";
+	var splines = req.body.splines;
+	
+	splines.forEach(function(spline) {
+		csvPath += `${spline.x},${spline.y},${spline.rotation};`;
+	});
+
+	request.post("http://localhost:8080/api/calculate_splines", {body: csvPath}, function(error, response, body) {
+		if (error || body == "no") {
+			console.log(csvPath);
+			res.send({status: false, error: error});
+			return;
+		}
+
+		console.log(body);
+		try {
+			res.send(JSON.parse(body));	
+		} catch (err) {
+			res.send({status: false, error: err});
+		}	
+	});
+});
+
+app.listen("8081", function() {
 	console.log("ok and good");
 });
--- static/js/app.js
+++ static/js/app.js
@@ -1,27 +1,20 @@
-const fieldWidth = 648; // inches
-const fieldHeight = 324; // inches
-const xOffset = 120;
-const yOffset = 180;
-const width = 1604; //pixels
-const height = 651; //pixels
-const widthString = `${width / 1.5}px`;
-const heightString = `${height / 1.5}px`
+var animation;
 
-const robotWidth = 31; // inches
-const robotHeight = 28.5; // inches
+function uuidv4() {
+	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+		var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+		return v.toString(16);
+	});
+}
 
-const waypointRadius = 7;
-const splineWidth = 2;
-
-const kEps = 1E-9;
-const pi = Math.PI;
-
-const angleInputOptions = {
-	max: 360,       // maximum value
-	min: 0,         // minimum value
-	step: 1,        // [min, min+step, ..., max]
-	name: 'angle',  // used for <input name>
-};
+function epicFail(msg) {
+	var bid = uuidv4();
+	$("body").append(`<div id="crashHandler-${bid}" title="Crash Handler (${bid})">${msg}</div>`)
+	$(`#crashHandler-${bid}`).dialog({
+		height: 500,
+		width: 1000
+	});
+}
 
 class Translation2d {
 	constructor(x, y) {
@@ -89,25 +82,6 @@ class Translation2d {
 
 	distance(other) {
 		return this.inverse().translateBy(other).norm();
-	}
-
-	draw(color, radius) {
-		color = color || "#2CFF2C";
-		ctx.beginPath();
-		ctx.arc(this.drawX, this.drawY, radius, 0, 2 * Math.PI, false);
-		ctx.fillStyle = color;
-		ctx.strokeStyle = color;
-		ctx.fill();
-		ctx.lineWidth = 0;
-		ctx.stroke();
-	}
-
-	get drawX() {
-		return (this.x + xOffset) * (width / fieldWidth);
-	}
-
-	get drawY() {
-		return height - (this.y + yOffset) * (height / fieldHeight);
 	}
 }
 
@@ -265,24 +239,6 @@ class Pose2d {
 	heading(other) {
 		return Math.atan2(this.translation.y - other.translation.y, this.translation.x - other.translation.x);
 	}
-
-	draw(drawHeading, radius) {
-		this.translation.draw(null, radius);
-
-		if (!drawHeading) {
-			return;
-		}
-
-		var x = this.translation.drawX;
-		var y = this.translation.drawY;
-
-		ctx.beginPath();
-		ctx.moveTo(x, y);
-		ctx.lineTo(x + 25 * Math.cos(-this.rotation.getRadians()), y + 25 * Math.sin(-this.rotation.getRadians()));
-		ctx.lineWidth = 3;
-		ctx.stroke();
-		ctx.closePath();
-	}
 	
 	toString() {
 		return `new Pose2d(new Translation2d(this.translation.x, this.translation.y), new Rotation2d(${this.rotation.cos}, ${this.rotation.sin}, ${this.rotation.normalize}))`;
@@ -303,94 +259,417 @@ function r2d(r) {
 	return r * (180 / Math.PI);
 }
 
-/** EPIC KNOCKOUT CODE **/
-ko.bindingHandlers.storeElement = {
-	init: function(element, valueAccessor) {
-		valueAccessor()(element);
+class Spline {
+	constructor() {
+
+	}
+
+	getPoint(t) {
+
+	}
+
+	getHeading(t) {
+
+	}
+
+	getCurvature(t) {
+
+	}
+
+	getDCurvature(t) {
+
+	}
+
+	getVelocity(t) {
+
+	}
+
+	getPose2d(t) {
+		return new Pose2d(this.getPoint(t), this.getHeading(t));
+	}
+
+	getPose2dWithCurvature(t) {
+		return new this.getPose2dWithCurvature(this.getPose2d(t), this.getCurvature(t), this.getDCurvature(t) / this.getVelocity(t));
 	}
 }
 
-function SnailPathModel() {
-	this.createPoint = function() {
-
-	}.bind(this);
-
-	this.update = function() {
-
-	}.bind(this);
-
-	this.animate = function() {
-
-	}.bind(this);
-
-	this.flipField = function() {
-		if (this.imageIsFlipped) {
-			this.backgroundCtx.drawImage(this.bgImageFlipped, 0, 0, width, height);
-		} else {
-			this.backgroundCtx.drawImage(this.bgImage, 0, 0, width, height);
-		}
-		this.imageIsFlipped = !this.imageIsFlipped;
-	}.bind(this);
-
-	this.newPathFile = function() {
-		var filename = prompt("Filename?");
-		$.post("/api/pathfiles", {
-			name: filename,
-			success: this.pullFiles
-		});
-	}.bind(this);
-
-	this.pullFiles = function() {
-		this.pathFiles([]);
-		$.getJSON("/api/pathfiles", (data) => {
-			this.pathFiles(data.files);
-		});
-	}.bind(this);
-
-	this.backgroundCtx = ko.observable();
-	this.fieldCtx = ko.observable();
-
-	this.backgroundCanvas = ko.observable();
-	this.backgroundCanvas.subscribe(function initCanvas(element) {
-		element.setAttribute("width", widthString);
-		element.setAttribute("height", heightString);
-		var ctx = element.getContext("2d");
-		ctx.canvas.width = width;
-		ctx.canvas.height = height;
-		this.backgroundCtx = ctx;
-	}.bind(this));
-
-	this.fieldCanvas = ko.observable();
-	this.fieldCanvas.subscribe(function initCanvas(element) {
-		element.setAttribute("width", widthString);
-		element.setAttribute("height", heightString);
-		var ctx = element.getContext("2d");
-		ctx.canvas.width = width;
-		ctx.canvas.height = height;
-		ctx.clearRect(0, 0, width, height);
-		this.fieldCtx = ctx;
-	}.bind(this));
-
-	this.canvases = ko.observable();
-	this.canvases.subscribe(function initCanvas(element) {
-		element.setAttribute("width", widthString);
-		element.setAttribute("height", heightString);
-	});
-
-	this.waypoints = ko.observableArray();
-	this.pathFiles = ko.observableArray();
-	this.pathFile = ko.observable();
-
-	this.bgImageIsFlipped = false;
-	this.bgImage = new Image();
-	this.bgImage.src = '/img/field.png';
-	this.bgImageFlipped = new Image();
-	this.bgImageFlipped.src = '/img/fieldFlipped.png';
-	this.bgImage.onload = function() {
-		this.backgroundCtx.drawImage(this.bgImage, 0, 0, width, height);
-	}.bind(this);
-
-	this.pullFiles();
+class QuinticHermiteSpline extends Spline {
+	constructor(p0, p1) {
+		var scale = 1.2 * p0.getTranslation().distance(p1.getTranslation);
+		this.x0 = p0.getTranslation().x();
+        this.x1 = p1.getTranslation().x();
+        this.dx0 = p0.getRotation().cos() * scale;
+        this.dx1 = p1.getRotation().cos() * scale;
+        this.ddx0 = 0;
+        this.ddx1 = 0;
+        this.y0 = p0.getTranslation().y();
+        this.y1 = p1.getTranslation().y();
+        this.dy0 = p0.getRotation().sin() * scale;
+        this.dy1 = p1.getRotation().sin() * scale;
+        this.ddy0 = 0;
+		this.ddy1 = 0;
+		
+		this.computeCoefficients();
+	}
 }
 
-ko.applyBindings(new SnailPathModel());
+var app = angular.module("snailpath", []);
+
+app.controller("app", function($scope, $http) {
+	$scope.config = {
+		thisShouldNotBe: true
+	};
+
+	$scope.points = [];
+
+	function getConfig() {
+		$scope.getConfigPromise = $http({
+			method: "GET",
+			url: "/api/config"
+		});
+
+		$scope.getConfigPromise.then(
+			function success(res) {
+				$scope.config = res.data;
+			},
+	
+			function failure(res) {
+				epicFail(res.data);
+			}
+		);
+	}
+
+	getConfig();
+});
+
+app.controller("info", function($scope) {
+	$scope.seltab = "file";
+});
+
+app.controller("infoConfig", function($scope) {
+
+});
+
+app.controller("infoAbout", function($scope) {
+	$scope.aboutInfo = `SnailPath 1.0
+By Carver Harrison
+Based on code from Team 254`;
+});
+
+app.controller("infoFile", function($scope, $http) {
+	$scope.creatingFilename = "";
+	$scope.loadingFilename = "";
+	$scope.files = [];
+
+	function reloadFiles() {
+		$http({
+			method: "GET",
+			url: "/api/pathfiles",
+		}).then(
+			function success(res) {
+				$scope.files = res.data.files;
+			},
+
+			function failure(res) {
+				epicFail(res.data);
+			}
+		)
+	}
+
+	$scope.saveFile = function() {
+		$http({
+			method: "POST",
+			url: "/api/pathfiles",
+			data: {
+				name: $scope.creatingFilename,
+				points: $scope.points
+			}
+		}).then(
+			function success(res) {
+				$scope.loadingFilename = $scope.creatingFilename;
+			},
+
+			function failure(res) {
+				epicFail(res.data);
+			}
+		);
+	}
+
+	$scope.loadFile = function() {
+		$http({
+			method: "GET",
+			url: `/api/pathfile?name=${$scope.creatingFilename}`
+		}).then(
+			function success(res) {
+				$scope.points = res.data.points;
+			},
+
+			function failure(res) {
+				epicFail(res.data);
+			}
+		);
+	}
+
+	reloadFiles();
+});
+
+app.controller("infoReleaseNotes", function($scope) {
+
+});
+
+app.controller("infoQuickLinks", function($scope) {
+
+});
+
+app.controller("pointManager", function($scope) {
+	$scope.newPoint = function() {
+		if ($scope.points.length == 0) {
+			$scope.points.push({
+				id: uuidv4(),
+				x: 100,
+				y: 100,
+				rotation: 0,
+				name: "Initial Point",
+				enabled: true
+			});
+		} else {
+			$scope.points.push({
+				id: uuidv4(),
+				x: _.last($scope.points).x+50,
+				y: _.last($scope.points).y+50,
+				rotation: _.last($scope.points).rotation+50,
+				name: "",
+				enabled: true
+			});
+
+			$scope.draw();
+		}
+	};
+
+	$scope.deletePoint = function(point) {
+		$scope.points = _.without($scope.points, _.findWhere($scope.points, { id: point.id }));
+	};
+
+	$scope.swapPoint = function(point, pos) {
+		var v = $scope.points.findIndex(function(i) {
+			return i.id == point.id;
+		});
+
+		var a = $scope.points[v];
+		var b = $scope.points[v+pos];
+
+		$scope.points[v] = b;
+		$scope.points[v+pos] = a;
+	};
+
+	$scope.newPoint();
+});
+
+app.controller("canvases", function($scope, $http) {
+	function pointDrawX(point) {
+		return (point.x + $scope.config.offset.x) * ($scope.config.screen.width / $scope.config.field.width);
+	}
+
+	function pointDrawY(point) {
+		return $scope.config.screen.height - (point.y + $scope.config.offset.y) * ($scope.config.screen.height / $scope.config.field.height);
+	}
+
+	function drawRobot(position, heading) {
+		var h = heading;
+		var t = Math.atan2($scope.config.robot.height, $scope.config.robot.width);
+		var r = Math.sqrt(Math.pow($scope.config.robot.width, 2) + Math.pow($scope.config.robot.height, 2)) / 2;
+
+		var angles = [h + (Math.PI / 2) + t, h - (Math.PI / 2) + t, h + (Math.PI / 2) - t, h - (Math.PI / 2) - t];
+		var points = [];
+
+		angles.forEach(function(angle) {
+			var point = new Translation2d(position.translation.x + (r * Math.cos(angle)), position.translation.y + (r * Math.sin(angle)));
+			points.push(point);
+			drawPoint(point, Math.abs(angle - heading) < Math.PI / 2 ? "#00AAFF" : "#0066FF", $scope.config.spline.width);
+		});
+
+		console.log("Drawing done");
+	}
+
+	function fillRobot(position, heading, color) {
+		var previous = $scope.fgctx.globalCompositeOperation;
+		$scope.fgctx.globalCompositeOperation = "destination-over";
+	
+		var translation = position.translation;
+	
+		$scope.fgctx.translate(translation.drawX, translation.drawY);
+		$scope.fgctx.rotate(-heading);
+	
+		var w = $scope.config.robot.width * ($scope.config.screen.width / $scope.config.field.width);
+		var h = $scope.config.robot.height * ($scope.config.screen.height / $scope.config.field.height);
+
+		console.log([h,w]);
+		$scope.fgctx.fillStyle = color || "rgba(0, 0, 0, 0)";
+		$scope.fgctx.fillRect(-h / 2, -w / 2, h, w);
+	
+		$scope.fgctx.rotate(heading);
+		$scope.fgctx.translate(-translation.drawX, -translation.drawY);
+	
+		$scope.fgctx.globalCompositeOperation = previous;
+	}
+
+	function drawPoint(point, color, radius) {
+		color = color || "#2CFF2C";
+		$scope.fgctx.beginPath();
+		$scope.fgctx.arc(pointDrawX(point), pointDrawY(point), radius, 0, 2 * Math.PI, true);
+		$scope.fgctx.fillStyle = color;
+		$scope.fgctx.strokeStyle = color;
+		$scope.fgctx.fill();
+		$scope.fgctx.lineWidth = 0;
+		$scope.fgctx.stroke();
+		console.log(`pointdraw [${[pointDrawX(point), pointDrawY(point), radius, 0, 2 * Math.PI, false]}]`);
+	}
+
+	function calculateWaypoints() {
+		var waypoints = [];
+
+		$scope.points.forEach(function(point) {
+			if (point.enabled) {
+				console.log(`waypoint calc X: ${point.x} Y: ${point.y}`);
+				waypoints.push(new Pose2d(new Translation2d(point.x, point.y), Rotation2d.fromDegrees(point.rotation), point.name));
+			}
+		});
+
+		return waypoints;
+	}
+
+	function drawWaypoints() {
+		var waypoints = calculateWaypoints();
+
+		waypoints.forEach(function(waypoint) {
+			drawPoint(waypoint, true, $scope.config.waypoint.radius);
+			drawRobot(waypoint, waypoint.rotation.getRadians());
+		});
+	}
+
+	function drawSplines(fill, animate) {
+		$http({
+			method: "POST",
+			url: "/api/calculatesplines",
+			data: {
+				splines: $scope.points
+			}
+		}).then(
+			function success(res) {
+				drawSplinesReal(fill, animate, res.data.points);
+			},
+
+			function failure(res) {
+				epicFail(res.data);
+			}
+		);
+	}
+
+	function drawSplinesReal(fill, animate, sp) {
+		var splinePoints = [];
+
+		if (sp === undefined) return;
+		
+		sp.forEach(function(point) {
+			splinePoints.push(new Pose2d(new Translation2d(point.x, point.y), Rotation2d.fromDegrees(point.rotation), point.name));
+		});
+
+		animate = animate || false;
+		var i = 0;
+	
+		if (animate) {
+			clearInterval(animation);
+	
+			animation = setInterval(function() {
+				if (i === splinePoints.length) {
+					animating = false;
+					clearInterval(animation);
+					return;
+				}
+	
+				animating = true;
+	
+				var hue = Math.round(180 * (i++ / splinePoints.length));
+	
+				var splinePoint = splinePoints[i];
+				
+				var previous = $scope.fgctx.globalCompositeOperation;
+				fillRobot(splinePoint, splinePoint.rotation.getRadians(), `hsla(${hue}, 100%, 50%, 0.025)`);
+				$scope.fgctx.globalCompositeOperation = "source-over";
+				drawRobot(splinePoint, splinePoint.rotation.getRadians());
+				drawPoint(splinePoint, false, $scope.config.spline.width);
+				$scope.fgctx.globalCompositeOperation = previous;
+			}, 25);
+		} else {
+			splinePoints.forEach(function(splinePoint) {
+				drawPoint(splinePoint, false, $scope.config.spline.width);
+	
+				if (fill) {
+					var hue = Math.round(180 * (i++ / splinePoints.length));
+					fillRobot(splinePoint, splinePoint.rotation.getRadians(), `hsla(${hue}, 100%, 50%, 0.025)`);
+				} else {
+					drawRobot(splinePoint, splinePoint.rotation.getRadians());
+				}
+			});
+		}
+	}
+
+	$scope.fgel = document.getElementById("cvForeground");
+	$scope.bgel = document.getElementById("cvBackground");
+	$scope.fgctx = $scope.fgel.getContext("2d");
+	$scope.bgctx = $scope.bgel.getContext("2d");
+
+	$scope.flipped = false;
+	$scope.imageNormal = new Image();
+	$scope.imageNormal.src = '/img/field.png';
+	$scope.imageFlipped = new Image();
+	$scope.imageFlipped.src = '/img/fieldFlipped.png';
+	$scope.image = $scope.imageNormal;
+
+	$scope.flip = function() {
+		if ($scope.flipped) {
+			$scope.image = $scope.imageNormal;
+		} else {
+			$scope.image = $scope.imageFlipped;
+		}
+
+		$scope.flipped = !$scope.flipped;
+
+		$scope.draw();
+	};
+
+	$scope.animate = function() {
+		drawSplines(true, true);
+	};
+
+	$scope.drawBg = function() {
+		$scope.bgctx.clearRect(0, 0, $scope.bgctx.canvas.width, $scope.bgctx.canvas.height);
+		$scope.bgctx.drawImage($scope.image, 0, 0, $scope.bgctx.canvas.width, $scope.bgctx.canvas.height);
+	};
+
+	$scope.drawFg = function() {
+		$scope.fgctx.clearRect(0, 0, $scope.fgctx.canvas.width, $scope.fgctx.canvas.height);
+		drawWaypoints();
+		drawSplines(true, false);
+		drawSplines(false, false);
+	};
+
+	$scope.draw = function() {
+		$scope.drawBg();
+		$scope.drawFg();
+	}
+
+	$scope.getConfigPromise.then(function() {
+		$scope.fgctx.canvas.width = $scope.config.screen.width;
+		$scope.fgctx.canvas.height = $scope.config.screen.height;
+		$scope.bgctx.canvas.width = $scope.config.screen.width;
+		$scope.bgctx.canvas.height = $scope.config.screen.height;
+
+		$scope.fgel.style.width = $scope.config.string.width;
+		$scope.fgel.style.height = $scope.config.string.height;
+		$scope.bgel.style.width = $scope.config.string.width;
+		$scope.bgel.style.height = $scope.config.string.height;
+
+		$scope.draw();
+	});
+});
